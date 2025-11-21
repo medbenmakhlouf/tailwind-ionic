e7c57e264d6861e1f72d94c9a1fc2f17ca053213
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-11-22 00:25:45 +0100

    fix: update CSS utility values to use new tracking value syntax for consistency

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -33,7 +33,7 @@ import {MatCheckbox} from '@angular/material/checkbox';
     </mat-form-field>
 
     <div matBadge="4" matBadgeOverlap="false" class="mbg-text-font-sans mbg-text-font-semibold">Text with a badge</div>
-    <mat-checkbox class="mck-touch-target-display-flex"> click me </mat-checkbox>
+    <mat-checkbox class="mck-touch-target-display-flex mck-label-text-tracking-wider mck-label-text-tracking-0.5"> click me </mat-checkbox>
   `,
   imports: [
     MatButton,
